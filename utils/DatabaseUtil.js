0a2222a5b3d4bd91599814e7967b8828f789334f
於卓慧 <shupian@2dfire.com>
2018-01-25 19:52:49 +0800

    db change save

--- utils/DatabaseUtil.js
+++ utils/DatabaseUtil.js
@@ -1,52 +1,64 @@
 const sqlite3 = require('sqlite3').verbose();
 const serverRootDir = require('./FileUtils.js').serverRootDir;
 
-function excuteDB(cmd, params, callback) {
-    
-    var db = new sqlite3.cached.Database(serverRootDir + '/db.sqlite3');
-    console.log(cmd);
 
-    console.log(params);
-    
-    
-    db.run(cmd, params, callback);
 
-    db.close();
+function createDB () {
+    const db = new sqlite3.cached.Database(serverRootDir + '/db.sqlite3');
+
+    return db;
 }
 
 module.exports.createDatabase = async() => {
-    const createDatabaseSQL = "CREATE TABLE IF NOT EXISTS info ( \
-        id integer PRIMARY KEY autoincrement,\
-        version TEXT,\
-        frameworkName TEXT,\
-        featureName TEXT,\
-        changelog TEXT,\
-        environment TEXT,\
-        commitHash TEXT, \
-        UNIQUE(version, environment, frameworkName, commitHash) \
-      )"
-
-    excuteDB(createDatabaseSQL);
-}
-
-module.exports.queryDB = async (params) => {
-    const SQL = `SELECT * FROM info`;
-}
-
-module.exports.insertDB = async (params) => {
-    console.log(params);
+    const db = createDB();
     
+    const createDatabaseSQL = `CREATE TABLE IF NOT EXISTS Framework ( 
+        id integer PRIMARY KEY autoincrement,
+        version TEXT,
+        frameworkName TEXT,
+        featureName TEXT,
+        changelog TEXT,
+        environment TEXT,
+        commitHash TEXT 
+      );
+        CREATE UNIQUE INDEX IF NOT EXISTS framework_PRODUCTION_I ON Framework (version, environment, frameworkName);
+        CREATE UNIQUE INDEX IF NOT EXISTS framework_DEVELOPMENT_I ON Framework (commitHash, environment, frameworkName);
+      `
+
+    try {
+        db.serialize(() => {
+            db.run(createDatabaseSQL);
+        });  
+    } catch (error) {
+        console.log(error);
+        
+    }finally {
+        db.close();
+    }
+
+}
+
+module.exports.queryDB = async(params) => {
+    const SQL = `SELECT * FROM Framework `;
+}
+
+module.exports.insertDB = async(params, callback) => {
+    console.log(params);
+
     excuteDB(
-        "INSERT INTO info (environment, version, frameworkName, featureName, changelog, commitHash) VALUES (?, ?, ?, ?, ?, ?)",
-        ["string", 
-        "params.version",
-        "params.frameworkName", 
-        "params.featureName", 
-        "params.changelog", 
-        "params.commitHash"],
+        "INSERT INTO Framework (environment, version, frameworkName, featureName, changelog, commitHash) VALUES (?, ?, ?, ?, ?, ?)", [
+            "params.environment",
+            "params.version",
+            "params.frameworkName",
+            "params.featureName",
+            "params.changelog",
+            "params.commitHash"
+        ],
         (error) => {
             console.log(error);
-            
+
+            callback()
+
         }
     );
 }
